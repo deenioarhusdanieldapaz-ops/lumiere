e2c6e05f4fbf7530f2f5eae5c11513b39b48835f
lumiére acessórios <lumierelafemasterlumiereacesso@email.com>
2026-09-25 17:45:25 +0200

    Adiciona Firebase Cloud Messaging para notificacoes push

---
+++ firebase-config-sw.js
@@ -0,0 +1,9 @@
+self.LUMIERE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD8n3Igc9qD-ZVyC_ONdgFmCBaskoV8P_U",
+  authDomain: "lumiere-notificacoes.firebaseapp.com",
+  projectId: "lumiere-notificacoes",
+  storageBucket: "lumiere-notificacoes.firebasestorage.app",
+  messagingSenderId: "668210725053",
+  appId: "1:668210725053:web:c55dd5ad4762ab7e0a37d4",
+  measurementId: "G-90PHG64T77"
+};
